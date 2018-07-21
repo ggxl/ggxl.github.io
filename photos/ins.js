c5a94ba07868e838c556ea31286c0c112bfb32c2
gtoy <imailsite@163.com>
2018-07-21 16:20:40 +0800

    Site updated: 2018-07-21 16:20:39

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://gaoxueliang.com.cn/min_photos/' + data.link[i];
-          var src = 'http://gaoxueliang.com.cn/org_photos/' + data.link[i];
+          var minSrc = 'http://gaoxueliang.com.cn/photos/min_photos/' + data.link[i];
+          var src = 'http://gaoxueliang.com.cn/photos/org_photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
